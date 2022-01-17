b5e122c0daf552d1945e93d115af781d6af32d97
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-17 14:47:36 +0100

    todolist + add function vuejs

---
+++ js/script.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: '#app',
+    data: {
+        todoList: [
+            {
+                testo: 'Fare la Spesa',
+                done: true
+            },
+            {
+                testo: 'Fare la Spesa',
+                done: false
+            },
+            {
+                testo: 'Fare la Spesa',
+                done: false
+            },
+        ],
+        temporaryInputTodo: {
+            testo: '',
+            done: false
+        },
+    },
+    methods: {
+        addTodo: function () {
+            this.todoList.push(this.temporaryInputTodo);
+            this.temporaryInputTodo= {
+                testo: '',
+                done: false
+            }
+        }
+    }
+});
